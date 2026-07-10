7e265aea0c38670bc4f5ab4c12f0ca7408d250c3
Pamela Carvalho <pamelakaroline004@gmail.com>
2026-07-09 21:25:26 -0300

    Add files via upload

---
+++ Front-End/site_araraSurf/js/produto.js
@@ -0,0 +1,126 @@
+const produtos = [
+    {
+        id_produto: 1,
+        descricao_Produto: "Prancha Fish 01",
+        carrinho_da_imagem: "../img/prancha-fish-01.png",
+        valor_unitario: 2000.00,
+        id_secao: 1,
+        nome_secao: "FishBoard"
+    },
+    {
+        id_produto: 2,
+        descricao_Produto: "Prancha Fish 02",
+        carrinho_da_imagem: "../img/prancha-fish-02.png",
+        valor_unitario: 3000.00,
+        id_secao: 1,
+        nome_secao: "FishBoard"
+    },
+    {
+        id_produto: 3,
+        descricao_Produto: "Prancha Fish 03",
+        carrinho_da_imagem: "../img/prancha-fish-03.png",
+        valor_unitario: 1600.00,
+        id_secao: 1,
+        nome_secao: "FishBoard"
+    },
+    {
+        id_produto: 4,
+        descricao_Produto: "Prancha Fish 04",
+        carrinho_da_imagem: "../img/prancha-fish-04.png",
+        valor_unitario: 2000.00,
+        id_secao: 1,
+        nome_secao: "FishBoard"
+    },
+    {
+        id_produto: 5,
+        descricao_Produto: "Prancha Fish 05",
+        carrinho_da_imagem: "../img/prancha-fish-05.png",
+        valor_unitario: 2000.00,
+        id_secao: 1,
+        nome_secao: "FishBoard"
+    },
+
+    {
+        id_produto: 6,
+        descricao_Produto: "Prancha Long 01",
+        carrinho_da_imagem: "../img/long/long1.jpg",
+        valor_unitario: 89.90,
+        id_secao: 2,
+        nome_secao: "Longboard"
+    },
+    {
+        id_produto: 7,
+        descricao_Produto: "Prancha Long 02",
+        carrinho_da_imagem: "../img/long/long2.webp",
+        valor_unitario: 99.90,
+        id_secao: 2,
+        nome_secao: "Longboard"
+    },
+    {
+        id_produto: 8,
+        descricao_Produto: "Prancha Long 03",
+        carrinho_da_imagem: "../img/long/long3.jpg",
+        valor_unitario: 149.90,
+        id_secao: 2,
+        nome_secao: "Longboard"
+    },
+    {
+        id_produto: 9,
+        descricao_Produto: "Prancha Long 04",
+        carrinho_da_imagem: "../img/long/long4",
+        valor_unitario: 159.90,
+        id_secao: 2,
+        nome_secao: "Longboard"
+    },
+    {
+        id_produto: 10,
+        descricao_Produto: "Prancha Long 05",
+        carrinho_da_imagem: "../img/long/long5.jpg",
+        valor_unitario: 179.90,
+        id_secao: 2,
+        nome_secao: "Longboard"
+    },
+
+    {
+        id_produto: 11,
+        descricao_Produto: "Prancha Short 01",
+        carrinho_da_imagem: "../img/shortBoard/short01.jpg",
+        valor_unitario: 129.90,
+        id_secao: 3,
+        nome_secao: "ShortBoard"
+    },
+    {
+        id_produto: 12,
+        descricao_Produto: "Prancha Short 02",
+        carrinho_da_imagem: "../img/shortBoard/short02",
+        valor_unitario: 18.90,
+        id_secao: 3,
+        nome_secao: "ShortBoard"
+    },
+    {
+        id_produto: 13,
+        descricao_Produto: "Prancha Short 01",
+        carrinho_da_imagem: "../img/shortBoard/short03",
+        valor_unitario: 159.90,
+        id_secao: 3,
+        nome_secao: "ShortBoard"
+    },
+    {
+        id_produto: 14,
+        descricao_Produto: "Prancha Short 04",
+        carrinho_da_imagem: "../img/shortBoard/short04",
+        valor_unitario: 349.90,
+        id_secao: 3,
+        nome_secao: "ShortBoard"
+    },
+    {
+        id_produto: 15,
+        descricao_Produto: "Prancha Short 05",
+        carrinho_da_imagem: "../img/shortBoard/short05",
+        valor_unitario: 279.90,
+        id_secao: 3,
+        nome_secao: "ShortBoard"
+    }
+];
+
+export{produtos}
