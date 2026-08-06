8de3a342869e88f46f718b5ed467ba271b4339a0
Pamela Carvalho <pamelakaroline004@gmail.com>
2026-08-06 13:26:42 -0300

    CRUD pessoas

--- CRUDPessoas/src/app/app.routes.ts
+++ CRUDPessoas/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Lista } from './component/lista/lista';
+import { Formulario } from './component/formulario/formulario';
+import { HomeComponent } from './component/home-component/home-component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'cadastro',
+        component: Formulario
+
+    },
+    {
+        path: 'Lista',
+        component:Lista
+    }
+];
---
+++ CRUDPessoas/src/app/component/formulario/formulario.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Pessoa } from '../../models/pessoa';
+import { PessoaService } from '../../services/pessoa-service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario',
+  imports: [FormsModule],
+  templateUrl: './formulario.html',
+  styleUrl: './formulario.css',
+})
+export class Formulario {
+
+  id = 0
+  nome = ''
+  email = ''
+  cpf = 0.0
+  dataNascimento = ''
+
+  constructor(private pessoaService: PessoaService) { }
+
+  salvar() {
+
+    console.log(this.nome, this.email, this.cpf, this.dataNascimento)
+
+    this.pessoaService.adicionar({
+      
+      id: 0,
+      nome: this.nome,
+      email: this.email,
+      cpf: this.cpf,
+      dataNascimento: this.dataNascimento
+    })
+  }
+
+}
